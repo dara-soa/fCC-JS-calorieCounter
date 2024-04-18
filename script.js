7a5dd4a97e5e43a599dfa8e5dc098f66d5d35edd
Daraujo-Soares <144711354+dara-soa@users.noreply.github.com>
2024-04-18 07:37:21 -0300

    # created isInvalidInput() function

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-  
   const regex = /[+-\s]/g;
   return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+
 }
